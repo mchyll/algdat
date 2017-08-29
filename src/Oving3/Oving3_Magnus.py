2e882f6d8d3c419689d5d568947662a1a768358f
Magnus Conrad Hyll <magnus@hyll.no>
2017-08-29 12:12:39 +0200

    Moved Øving 3

--- src/Oving3/Oving3_Magnus.py
+++ src/Oving3/Oving3_Magnus.py
@@ -1,5 +1,5 @@
 def insertionsort(arr):
-    for i in range(1, len(arr) - 1):
+    for i in range(1, len(arr)):
         current = arr[i]
         j = i - 1
         while j >= 0 and arr[j] > current:
@@ -8,7 +8,10 @@ def insertionsort(arr):
         arr[j + 1] = current
 
 
-def quicksort(arr):
+def quicksort(arr, left = 0, right = -1):
+    if right == -1:
+        right = len(arr)
+
     if len(arr) <= 10:
         insertionsort(arr)
         return
@@ -16,7 +19,17 @@ def quicksort(arr):
     l = arr[0]
     m = arr[len(arr) / 2]
     r = arr[len(arr) - 1]
+    pivot = m
 
+    if pivot > l and pivot > r:
+        pivot = max(l, r)
+
+
+arr = [5,4,3,2,1]
+insertionsort(arr)
+print(arr)
+
+"""
     if l > m:
         temp = arr[0]
         l = arr[0] = m
@@ -26,3 +39,4 @@ def quicksort(arr):
         temp = arr[len(arr) / 2]
         m = arr[len(arr) / 2] = r
         r = arr[len(arr) - 1] = temp
+"""
