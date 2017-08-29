8b09503f1ec1c397809ad2bbc0efa1fd573e7781
Magnus Conrad Hyll <magnus@hyll.no>
2017-08-29 15:36:48 +0200

    Funker ikke, pushe læll

--- src/Oving3/Oving3_Magnus.py
+++ src/Oving3/Oving3_Magnus.py
@@ -1,36 +1,81 @@
-def insertionsort(arr):
-    for i in range(1, len(arr)):
+def insertionsort(arr, left = 0, right = -1):
+    if right < 0:
+        right = len(arr)
+
+    for i in range(left + 1, right):
         current = arr[i]
         j = i - 1
-        while j >= 0 and arr[j] > current:
+        while j >= left and arr[j] > current:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = current
 
 
 def quicksort(arr, left = 0, right = -1):
-    if right == -1:
+    if right < 0:
         right = len(arr)
 
-    if len(arr) <= 10:
-        insertionsort(arr)
+    if right - left <= 10:
+        insertionsort(arr, left, right)
         return
 
-    deleps = 0
-    quicksort(arr, left, deleps-1)
-    quicksort(arr, deleps + 1, right)
-    l = arr[0]
-    m = arr[len(arr) / 2]
-    r = arr[len(arr) - 1]
-    pivot = m
+    pivot = findmedian(arr, left, right)
+    pivotValue = arr[pivot]
 
-    if pivot > l and pivot > r:
-        pivot = max(l, r)
+    lowerPair = left
+    higherPair = right - 1
+    arr[pivot], arr[right - 1] = arr[right - 1], arr[pivot]  # Move the pivot out of the way
+
+    while True:
+        while arr[lowerPair] < pivotValue:
+            lowerPair += 1
+        while arr[higherPair] > pivotValue:
+            higherPair -= 1
+
+        if lowerPair >= higherPair:
+            break
+
+        arr[lowerPair], arr[higherPair] = arr[higherPair], arr[lowerPair]
+
+    pivot = higherPair
+    arr[pivot], arr[right - 1] = arr[right - 1], arr[pivot]
+
+    quicksort(arr, left, pivot - 1)
+    quicksort(arr, pivot + 1, right)
 
 
-arr = [5,4,3,2,1]
-insertionsort(arr)
+def findmedian(arr, left, right):
+    middle = (left + right) // 2
+    right -= 1
+
+    if arr[left] > arr[middle]:
+        arr[left], arr[middle] = arr[middle], arr[left]
+
+    if arr[left] > arr[right]:
+        arr[left], arr[right] = arr[right], arr[left]
+
+    if arr[middle] > arr[right]:
+        arr[middle], arr[right] = arr[right], arr[middle]
+
+    return middle
+
+
+def verifySortedArray(arr):
+    prev = arr[0]
+
+    for i in range(1, len(arr)):
+        if prev > arr[i]:
+            return False
+        prev = arr[i]
+
+    return True
+
+
+arr = [x for x in range(100, 0, -1)]
+quicksort(arr)
 print(arr)
+print("Er tabellen sortert korrekt? {}".format(verifySortedArray(arr)))
+
 
 """
     if l > m:
